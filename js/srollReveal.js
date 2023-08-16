bbbd0d5ea52b5e28e96785dd31e663ec83db6e00
Ciarango10 <carlosarango3101@gmail.com>
2023-08-15 20:57:27 -0500

    Cambios en landing page

---
+++ js/srollReveal.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+    sr.reveal('#contenedorInfo', {
+        duration: 3000
+    });
+    sr.reveal('#contenedorVideo', {
+        duration: 3000
+    });
+    sr.reveal('#tituloPrincipal', {
+        duration: 3000
+    });
+    sr.reveal('#flecha', {
+        duration: 3000
+    });
